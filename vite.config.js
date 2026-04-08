bdea54767c16f64fdd4c290f5234a2c483109305
PedroTello3810 <tello.3810@outlook.com>
2026-04-07 21:46:14 -0600

    update viteconfing

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/DigitalBank",
+  base: "/DigitalBank-FrontendMentor-/",
   server: {
     host: true,
     port: 5173,
